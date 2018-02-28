84b658fe872c4fa02735ca20fc1b44d460deb7f6
Kavita Kalaichelvan <kkavita92@gmail.com>
2018-02-28 16:05:13 +0000

    Setup Foundation within app

--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -47,7 +47,14 @@ exports.config = {
     babel: {
       // Do not use ES6 compiler in vendor code
       ignore: [/vendor/]
-    }
+    },
+    sass: {
+     options: {
+       includePaths: [
+         'node_modules/foundation-sites/scss',
+       ]
+     }
+   }
   },
 
   modules: {
@@ -57,6 +64,10 @@ exports.config = {
   },
 
   npm: {
-    enabled: true
+    enabled: true,
+    globals: {
+      $: 'jquery',
+      jQuery: 'jquery',
+    }
   }
 };
